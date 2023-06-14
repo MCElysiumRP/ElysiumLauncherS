fe7aa104e00da5c6fbef91ca16eef8245ae0d9b8
Alouchi <117847034+MoiLaVie@users.noreply.github.com>
2023-06-14 21:47:30 +0200

    Update Initial

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,19 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Bienvenue sur", "author": "Elysium" },
+            { "message": "Launcher by", "author": "Alouchi" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
